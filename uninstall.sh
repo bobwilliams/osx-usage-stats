c250b50229ca5a0d7cfa31b64b7cc5146071589d
Bob Williams <bobwilliams.ii@gmail.com>
2013-06-06 13:51:14 -0400

    adding uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+DIR_LOCATION='/Users/johndebovis/workspace/osx-usages-stats'
+
+sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
+sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.ws_monitor.plist
