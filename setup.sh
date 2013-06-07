b3b33a449029f1b374ff65bf8ade1e62bbf1ad80
Bob Williams <bobwilliams.ii@gmail.com>
2013-06-07 15:20:08 -0400

    fixing .plist pointing to wrong apps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 DIR_LOCATION='/Users/bob/workspace/osx-usage-stats'
 
-sudo chown root $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
-sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
-sudo launchctl load $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
-sudo launchctl start com.sparcedge.memory-stats
-sudo launchctl list | grep com.sparcedge.memory-stats
+sudo chown root $DIR_LOCATION/misc/com.sparcedge.memory_stats.plist
+sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.memory_stats.plist
+sudo launchctl load $DIR_LOCATION/misc/com.sparcedge.memory_stats.plist
+sudo launchctl start com.sparcedge.memory_stats
+sudo launchctl list | grep com.sparcedge.memory_stats
 
 sudo chown root $DIR_LOCATION/misc/com.sparcedge.ws_monitor.plist
 sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.ws_monitor.plist
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 DIR_LOCATION='/Users/bob/workspace/osx-usage-stats'
 
-sudo chown root $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
-sudo launchctl stop com.sparcedge.memory-stats
-sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.memory-stats.plist
+sudo chown root $DIR_LOCATION/misc/com.sparcedge.memory_stats.plist
+sudo launchctl stop com.sparcedge.memory_stats
+sudo launchctl unload $DIR_LOCATION/misc/com.sparcedge.memory_stats.plist
 
 sudo chown root $DIR_LOCATION/misc/com.sparcedge.ws_monitor.plist
 sudo launchctl stop com.sparcedge.ws_monitor
